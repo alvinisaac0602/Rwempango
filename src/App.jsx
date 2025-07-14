d980cef3cd302e30f89b8547af63341596f604bd
alvinisaac0602 <kiizaisaacalvin256@gmail.com>
2025-07-14 12:07:34 +0300

    Setting up routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,33 @@
 import { useState } from "react";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  BrowserRouter,
+} from "react-router-dom";
+import Finance from "./Pages/Finance";
+import Agricproducts from "./Pages/Agricproducts";
+import Realestate from "./Pages/Realestate";
+import Home from "./Pages/Home";
+import Contact from "./Pages/Contact";
+import Header from "./Components/Header";
+import Footer from "./Components/Footer";
 
 function App() {
   return (
-    <>
-      <p className="text-3xl font-bold underline text-center mt-10">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <BrowserRouter>
+      <Header />
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/agricproducts" element={<Agricproducts />} />
+        <Route path="/realestate" element={<Realestate />} />
+        <Route path="/finance" element={<Finance />} />
+      </Routes>
+
+      <Footer />
+    </BrowserRouter>
   );
 }
 
